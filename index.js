ca6a20eb8c5e7828dc79d0906c15844f905c7caa
Icaro Caldeira <icaro.caldeira@hotmail.com>
2017-12-21 03:03:30 -0200

    revert errors

--- index.js
+++ index.js
@@ -7,26 +7,24 @@
  * @returns {string} Formatted text
  */
 function format (data, mask){
-    if (!mask) return data;
+  if (!mask) return data;
 
-    var text = '';
-    for (var i = 0, x = 1; x && i < mask.length; ++i) {
-        var c = data.charAt(i);
-        var m = mask.charAt(i);
+  var text = '';
+  var c, m, i, x;
 
-        if (data.length == 1 && i == 0 && i + 1 < mask.length && /^((?!(#|A|N|X)).)*/.test(m)) {
-            text += m;
-            m = mask.charAt(i + 1);
-        }
+  for (i = 0, x = 1; x && i < mask.length; ++i) {
+    c = data.charAt(i);
+    m = mask.charAt(i);
 
-        switch (m) {
-            case '#' : if (/\d/.test(c))        {text += c;} else {x = 0;} break;
-            case 'A' : if (/[a-z]/i.test(c))    {text += c;} else {x = 0;} break;
-            case 'N' : if (/[a-z0-9]/i.test(c)) {text += c;} else {x = 0;} break;
-            case 'X' : text += c; break;
-            default  : text += m; break;
-        }
+    switch (m) {
+      case '#' : if (/\d/.test(c))        {text += c;} else {x = 0;} break;
+      case 'A' : if (/[a-z]/i.test(c))    {text += c;} else {x = 0;} break;
+      case 'N' : if (/[a-z0-9]/i.test(c)) {text += c;} else {x = 0;} break;
+      case 'X' : text += c; break;
+      default  : text += m; break;
+    }
   }
+
   return text;
 }
 
